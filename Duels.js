da6de576f80d11632f36ff0905a3dc15a10ae18f
Macofan <67532977+Macofan@users.noreply.github.com>
2021-05-20 18:54:19 -0300

    Añadidas variables howTo y skills con placeholder code, y un prompt que no anda

--- Duels.js
+++ Duels.js
@@ -1,7 +1,27 @@
+    //Aca se definen variables
+
+//El menu principal
 let welcome = `Bienvenido a DUELS!
 1 Como jugar
 2 Guerrero
 3 Picaro
 4 Paladin
-5 Caballero de la Muerte`
+5 Caballero de la Muerte
+6 Muñeco de entrenamiento`
     
+// El 'como jugar', añadir cuando sepa bien como funciona el juego
+let howTo = `
+Te curas y ganas
+`
+
+// skill/s, help, ayuda, habilidades y "como jugar" todas te llevan aca. Agregar todas las habilidades cuando ya tenga el codigo hecho.
+let skills = `
+1 Shield
+2 Heal
+`
+
+
+    //Fin de las variables, a partir de aca es codigo de verdad
+
+console.log(welcome);
+
